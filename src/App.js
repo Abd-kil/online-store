1f2beda0eafb9b3979045a053e065baeb4fa66e2
abdulrahman <abood.kil2003123@gmail.com>
2023-10-02 21:11:16 +0300

    some fixes to gh-page

--- src/App.js
+++ src/App.js
@@ -6,11 +6,12 @@ import Contact from './pages/Contact';
 import NoPage from './pages/NoPage';
 import NavBar from './components/NavBar';
 function App() {
+  const baseUrl = process.env.PUBLIC_URL;
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={baseUrl}>
       <Routes>
-        <Route path='/' element={<NavBar/>}>
-          <Route index element={<Home/>}/>
+        <Route element={<NavBar/>}>
+          <Route exact index path='/' element={<Home/>}/>
           <Route path="/cart" element={<Cart/>}/>
           <Route path="/contact" element={<Contact/>}/>
           <Route path="*" element={<NoPage/>}/>
--- src/components/Product.js
+++ src/components/Product.js
@@ -1,5 +1,6 @@
 import logo from '../logo.svg';
 function Product(){
+    let rate = 4;
     return(
         <div className='product'>
             <div style={{background:'#eee'}}>
@@ -8,11 +9,11 @@ function Product(){
             <h4>React component</h4>
             <h5>$ 399</h5>
             <div className="rate-box">
-            <i className='bx bxs-star' style={{color:'orange'}}></i>
-            <i className='bx bxs-star' style={{color:'orange'}}></i>
-            <i className='bx bxs-star' style={{color:'orange'}}></i>
-            <i className='bx bxs-star' style={{color:'orange'}}></i>
-            <i className='bx bxs-star' style={{color:'#ccc'}}></i>
+            <i className='bx bxs-star' style={rate >= 1?{color:'orange'}:{color:'#ccc'}}></i>
+            <i className='bx bxs-star' style={rate >= 2?{color:'orange'}:{color:'#ccc'}}></i>
+            <i className='bx bxs-star' style={rate >= 3?{color:'orange'}:{color:'#ccc'}}></i>
+            <i className='bx bxs-star' style={rate >= 4?{color:'orange'}:{color:'#ccc'}}></i>
+            <i className='bx bxs-star' style={rate >= 5?{color:'orange'}:{color:'#ccc'}}></i>
         </div>
         </div>
     )
