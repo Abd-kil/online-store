41670a5b2bb7665ec015d365f2acd9fa49dc6f76
abdulrahman <abood.kil2003123@gmail.com>
2023-10-02 22:33:34 +0300

    fixes

--- src/App.js
+++ src/App.js
@@ -6,12 +6,11 @@ import Contact from './pages/Contact';
 import NoPage from './pages/NoPage';
 import NavBar from './components/NavBar';
 function App() {
-  const baseUrl = process.env.PUBLIC_URL;
   return (
-    <BrowserRouter basename={baseUrl}>
+    <BrowserRouter>
       <Routes>
         <Route element={<NavBar/>}>
-          <Route exact index path='/' element={<Home/>}/>
+          <Route exact path='/' element={<Home/>}/>
           <Route path="/cart" element={<Cart/>}/>
           <Route path="/contact" element={<Contact/>}/>
           <Route path="*" element={<NoPage/>}/>
